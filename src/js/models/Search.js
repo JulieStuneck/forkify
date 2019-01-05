b1288e8f05f8fe575dd259b42e084c82ad116aa2
JulieStuneck <jbstuneck@yahoo.com>
2019-01-04 21:15:01 -0500

    api calls to get recipe details

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios'; //fetch not supported in older browsers so will use axios. Axios also automatically returns Json.
+import { key, proxy } from '../config';
 
 export default class Search {
 	constructor(query) {
@@ -6,11 +7,9 @@ export default class Search {
 	}
 
 	async getResults() {
-	//const proxy = 'https://cors-anywhere.herokuapp.com/';
-		const key = '41935e00cb1cce8120141e1d8734722f';
 		try {
-			const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`) //q is coming from api specs
-			//const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+			//const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`) //q is coming from api specs
+			const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
 			//console.log(res);
 			this.result = res.data.recipes;
 			//console.log(this.result);
